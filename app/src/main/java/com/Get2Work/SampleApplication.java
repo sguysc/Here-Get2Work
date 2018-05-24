3b1e79a73d4e3a27c25eb65ef734fd3ae0e64dfd
sguysc@gmail.com <Gs1234dc>
2018-05-25 00:21:52 +0300

    Get2Work

---
+++ app/src/main/java/com/Get2Work/SampleApplication.java
@@ -0,0 +1,28 @@
+package com.Get2Work;
+
+import android.support.multidex.MultiDexApplication;
+
+import com.here.mobility.sdk.core.HereMobilitySdk;
+
+
+/**********************************************************
+ * Copyright © 2018 HERE Global B.V. All rights reserved. *
+ **********************************************************/
+public class SampleApplication extends MultiDexApplication {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //HereMobilitySdk initialization must be called in onCreate() method to initialize the SDK.
+        HereMobilitySdk.init(this);
+
+        //Returns whether the current process is the SDK agent process.
+        if (HereMobilitySdk.isHereAgentProcess(this)){
+            return;
+        }
+
+        //put here the rest of you app sdks initialization.
+    }
+}
